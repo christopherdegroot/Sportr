a869ed2f80df0858066600365efb5a066c54968f
Noah V <noah@noahvandenberg.com>
2022-03-18 17:13:37 -0700

    add storybook and configure it to run with nextjs

---
+++ stories/pages/home.stories.jsx
@@ -0,0 +1,9 @@
+import Home from "../../pages/index";
+import "../../styles/globals.css";
+
+export default {
+  title: "Pages/Home",
+  component: Home,
+};
+
+export const HomePage = () => <Home />
