0ec46cf8e888cb792a1fabe240d2ae39469b403b
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-19 12:09:57 -0700

    add forms for email and password

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,21 +1,31 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import styles from '../../styles/login-header.module.css'
+import MyButton from '../../components/MyButton'
 
-export default function Login(home) {
+
+export default function Login(props) {
   return (
     <>
       <Head>
         <title>Login</title>
       </Head>
+      <div className={styles.loginHeader}>
       <h1>Login</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-      {!home && (
+      <div>
+      <form className={styles.form}>
+        <input type="text" placeholder='Email'></input>
+        <input type="text" placeholder='Password'></input>
+        <Link href="/Profile">
+              <a> <MyButton name={'Submit'}/></a>
+            </Link>
+      </form>
+      </div>
+      <br></br>
+      </div>
+      {!props.home && (
         <div className={styles.backToHome}>
-          <Link href="/">
+         <Link href="/">
             <a>← Back to home</a>
           </Link>
         </div>
