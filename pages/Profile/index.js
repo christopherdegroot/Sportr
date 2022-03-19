38e1ea86cbf2b10e9635387c9d39586d765b16bf
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-19 13:37:01 -0700

    create chakra layout for profile page

--- pages/Profile/index.js
+++ pages/Profile/index.js
@@ -1,18 +1,33 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import { Container, Flex, VStack } from '@chakra-ui/react'
 
 export default function Userhome() {
   return (
     <>
-      <Head>
-        <title>Welcome Home</title>
-      </Head>
-      <h1>Welcome Home</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to to login</a>
-        </Link>
-      </h2>
+    <Container maxW="container.xl" p={0}>
+      <Flex h='100vh' py={20}>
+        <VStack 
+        w="full" 
+        h="full" 
+        p={10} 
+        spacing={10} 
+        alignItems="flex-start" 
+        >
+
+        </VStack>
+        <VStack 
+        w="full" 
+        h="full" 
+        p={10} 
+        spacing={10} 
+        alignItems="flex-start" 
+        bg="gray.50"
+        >
+
+        </VStack>
+      </Flex>
+    </Container>  
     </>
   )
 }
