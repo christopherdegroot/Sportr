9679a3cbd5998c01a8d147a6aad0af4ba90b364b
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-19 12:10:20 -0700

    remove button

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -12,11 +12,7 @@ export default function FirstPost() {
       </Head>
     
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
+   
     </Layout>
   )
 }
