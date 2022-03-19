0693a771b57f8451aa8a79c1cb3b380bdb8f685a
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-19 13:20:54 -0700

    add chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 // We can use this page to keep state when navigating between pages
 // Also alters global styles
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+   </ChakraProvider>
+  );
+};
 
 export default MyApp
