6176b072a67cd2b69e9f96a4f2e7c5c698ea8070
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-18 22:49:56 -0700

    add button component

---
+++ components/myButton.js
@@ -0,0 +1,12 @@
+import styles from './MyButton.module.css'
+
+export default function MyButton(props) {
+
+  return (
+    <div className={styles.MyButton}> 
+    <button >
+    Placeholder
+  </button></div>
+   
+   
+  )}
