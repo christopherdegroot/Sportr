b51b4d1928b76fb69753c4087ae88f90ab2fc942
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-20 13:30:24 -0700

    change login form on slide to be within container, limited width

--- components/LoginModule.js
+++ components/LoginModule.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import {
   CloseButton,
   ButtonGroup,
+  Container,
   Checkbox,
   Button,
   Select,
@@ -19,33 +20,33 @@ const LoginModule = (props) => {
   const onClose = props.onClose;
   return (
     <>
-      <CloseButton
-        onClick={() => {
-          onClose();
-        }}
-      ></CloseButton>
-      <VStack>
-        <Heading>Please Log In</Heading>
-        <SimpleGrid columns={1} rowGap={6} w="full">
-          <GridItem colSpan={2}>
-            <FormControl>
-              <FormLabel>Email</FormLabel>
-              <Input type="email" placeholder="Email Address" />
-            </FormControl>
-          </GridItem>
-          <GridItem colSpan={2}>
-            <FormControl>
-              <FormLabel>Password</FormLabel>
-              <Input type="password" placeholder="Password" />
-            </FormControl>
-          </GridItem>
-        </SimpleGrid>
-        <Link href="/profile">
-          <Button w="9em" type="submit" name={"Submit"} variant="outline">
-            Submit
-          </Button>
-        </Link>
+    <VStack flexDirection='row-reverse'>
+      <CloseButton onClick={() => {onClose();}}></CloseButton>
       </VStack>
+      <Container maxW="20em" flexDirection='column' justifyContent='center'>
+        <VStack flexDirection='column' >
+          <Heading>Please Log In</Heading>
+          <SimpleGrid columns={1} rowGap={2} w="full">
+            <GridItem colSpan={2}>
+              <FormControl>
+                <FormLabel>Email</FormLabel>
+                <Input type="email" placeholder="Email Address" />
+              </FormControl>
+            </GridItem>
+            <GridItem colSpan={2}>
+              <FormControl>
+                <FormLabel>Password</FormLabel>
+                <Input marginBottom='1.5em' type="password" placeholder="Password" />
+              </FormControl>
+            </GridItem>
+          </SimpleGrid>
+          <Link href="/profile">
+            <Button w="9em" type="submit" name={"Submit"} variant="outline">
+              Submit
+            </Button>
+          </Link>
+        </VStack>
+      </Container>
     </>
   );
 };
