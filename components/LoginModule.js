3f444a1e9728fe4efaf0c1b79e3eb4c1d6ba1351
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-20 13:16:37 -0700

    prettier

--- components/LoginModule.js
+++ components/LoginModule.js
@@ -1,33 +1,53 @@
 import Link from "next/link";
-import { CloseButton, ButtonGroup, Checkbox, Button, Select, FormControl, FormLabel, Input, VStack, Heading, Text, SimpleGrid, GridItem } from "@chakra-ui/react";
+import {
+  CloseButton,
+  ButtonGroup,
+  Checkbox,
+  Button,
+  Select,
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+  Heading,
+  Text,
+  SimpleGrid,
+  GridItem,
+} from "@chakra-ui/react";
 
 const LoginModule = (props) => {
-  const onClose = props.onClose
+  const onClose = props.onClose;
   return (
     <>
-      <CloseButton onClick={()=>{onClose()}}></CloseButton>
-    <VStack>
+      <CloseButton
+        onClick={() => {
+          onClose();
+        }}
+      ></CloseButton>
+      <VStack>
         <Heading>Please Log In</Heading>
-            <SimpleGrid columns={1} rowGap={6} w='full'>
-              <GridItem colSpan={2}>
-                  <FormControl>
-                    <FormLabel>Email</FormLabel>
-                    <Input type='email' placeholder="Email Address" />
-                  </FormControl>
-              </GridItem>
-              <GridItem colSpan={2}>
-                <FormControl>
-                  <FormLabel>Password</FormLabel>
-                  <Input type='password' placeholder="Password" />
-                </FormControl>
-              </GridItem>
-            </SimpleGrid>
-            <Link href="/profile">
-              <Button w='9em' type='submit' name={'Submit'} variant='outline'>Submit</Button>
-            </Link>
-          </VStack>
-          </>
-      )
-}
+        <SimpleGrid columns={1} rowGap={6} w="full">
+          <GridItem colSpan={2}>
+            <FormControl>
+              <FormLabel>Email</FormLabel>
+              <Input type="email" placeholder="Email Address" />
+            </FormControl>
+          </GridItem>
+          <GridItem colSpan={2}>
+            <FormControl>
+              <FormLabel>Password</FormLabel>
+              <Input type="password" placeholder="Password" />
+            </FormControl>
+          </GridItem>
+        </SimpleGrid>
+        <Link href="/profile">
+          <Button w="9em" type="submit" name={"Submit"} variant="outline">
+            Submit
+          </Button>
+        </Link>
+      </VStack>
+    </>
+  );
+};
 
 export default LoginModule;
