ea3d28e0b5f26926addd5758006ea570ce45108a
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-20 17:46:37 -0700

    create sportEvent component

---
+++ components/SportEvent.js
@@ -0,0 +1,19 @@
+import { Container, VStack, Heading } from "@chakra-ui/react";
+
+export default function SportEvent(props) {
+  return(
+    <VStack 
+      w="full" 
+      h="full" 
+      p={10} 
+      spacing={10} 
+      alignItems="center" 
+    >
+      <Heading>
+        Event Feed
+      </Heading>
+      
+      
+    </VStack>
+  )
+}
