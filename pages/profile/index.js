7ecd251ccda12a3e8998acbdd734077a216ecc30
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-20 20:10:37 -0700

    move back to home button to external component

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,8 +1,8 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import { Container, Flex, VStack } from '@chakra-ui/react'
+import { Box, Container, Flex, VStack } from '@chakra-ui/react'
+import EventFeed from '../../components/EventFeed'
 import styles from '../../styles/login-header.module.css'
-import SportEvent from '../../components/SportEvent'
 
 
 export default function Userhome(props) {
@@ -10,20 +10,12 @@ export default function Userhome(props) {
     <>
     <Container maxW="container.xl" p={0}>
       <Flex h='100vh' py={20}>
-      <SportEvent>
-        
-      </SportEvent>
+      <EventFeed>
+
+      </EventFeed>
       </Flex>
     </Container>
-    <div>
-      {!props.home && (
-        <div className={styles.backToHome}>
-         <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
-    </div>  
+   
     </>
   )
 }
