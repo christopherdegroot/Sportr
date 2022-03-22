a06b7bbd272c9f7d6b3e63295f5eb1fae6dae971
Noah V <noah@noahvandenberg.com>
2022-03-22 12:44:22 -0700

    add user object to the profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,45 +1,46 @@
-import Link from "next/link";
-import Head from "next/head";
 import {
-  Button,
   Text,
   Image,
   Heading,
   Box,
   Container,
-  Flex,
   VStack,
-  Slide,
-  useDisclosure,
-  Collapse
 } from "@chakra-ui/react";
-import EventFeed from "../../components/EventFeed";
-import styles from "../../styles/login-header.module.css";
-import LoginModule from "../../components/LoginModule";
-import RegisterModule from "../../components/RegisterModule";
 import NavigationBar from "../../components/NavigationBar";
 
 export default function Userhome(props) {
-  const { isOpen, onClose, onToggle } = useDisclosure()
+  const userObject = {
+    id:4,
+    email:"noahthedev@hotmail.com",
+    name:"Noah V",
+    password:"haha",
+    bio:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris purus nisl, faucibus nec laoreet ultricies, fringilla et dolor. Pellentesque id odio vehicula, mollis nibh in, congue tortor. Etiam pellentesque sem.",
+    profile_image_url:"https://www.boredpanda.com/blog/wp-content/uploads/2021/08/funny-monkeys-56-612393fdd1081__700.jpg",
+    gender:"male",
+    birthdate:'1999-12-31',
+    sports: ['basketball', 'soccer', 'running', 'spikeball'],
+    km_range:10
+  }
+  const {id, email, name, passsword, bio, profile_image_url, gender, birthdate, sports, km_range} = userObject
 
   return (
     <>
       {/* Profile Preview */}
-      <Container pt='10' maxW="container.md">
+      <Container pt='10' mb='100' maxW="container.md">
       <Box>
       </Box>
         <Box rounded="md" borderWidth="1px" borderRadius="lg" overflow="hidden">
           <VStack
             flexDirection="column"
-            justifyContent="flex-start"
-            alignItems="flex-start"
+            justify="flex-start"
+            align="flex-start"
             w="full"
             h="full"
             p={10}
           >
             <VStack
               w="full"
-              flexDirection={"row"}
+              flexDirection={"column"}
               justifyContent={"flex-start"}
             >
               <Image
@@ -49,30 +50,9 @@ export default function Userhome(props) {
                 src="https://bit.ly/dan-abramov"
                 alt="Dan Abramov"
               />
-              <Heading pl={5}>Dan Abramov</Heading>
-              <Text pt={'2'} fontSize="2xl">, 27</Text>
+              <Heading pl={5}>{name}</Heading>
             </VStack>
-            <Text fontSize="xl">
-              I am a software developer working on React. I like playing
-              spikeball, tennis, and volleyball. I am looking for new friends to
-              play these sports with in Vancouver as I just moved here.
-            </Text>
-          </VStack>
-          <VStack flexDirection={"row"} justifyContent={"flex-end"}>
-            <Box flexDirection={'column'} justifyContent={'flex-end'} pr={5} pb={5}>
-              <Button onClick={onToggle}  colorScheme="teal" variant={'outline'}>Update</Button>
-              <Collapse in={isOpen} animateOpacity>
-                <Box
-                  p='40px'
-                  mt='4'
-                  rounded='md'
-                  shadow="md"
-                  >
-                  <RegisterModule onClose={onClose}>
-                  </RegisterModule>
-                </Box>
-              </Collapse>
-            </Box>
+            <Text fontSize="xl" align='center'>{bio}</Text>
           </VStack>
         </Box>
       </Container>
