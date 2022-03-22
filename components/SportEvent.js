399201f185ca6dab0eae750f1aa7b25022a71c12
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-22 14:18:55 -0700

    styling overhaul

--- components/SportEvent.js
+++ components/SportEvent.js
@@ -17,60 +17,63 @@ import React from "react";
 
 function SportEvent(props) {
   const onClose = props.onClose;
-  console.log('props onclose', props.findEvents)
   const [show, setShow] = React.useState(true)
   return (
     <>
      {show && (<Box
         _hover={{ boxShadow: "xl" }}
-        p="6"
         rounded="md"
         bg="white"
         borderWidth="2px"
         borderRadius="lg"
         overflow="hidden"
       >
-        <Image src={props.event.imageUrl} />
-
-        <Box  p="6">
-          <Box display="flex" alignItems="baseline">
+        <Image src={props.event.imageUrl}/>
+        <Box pl='2'>
+          <Box pl='1' pt={5} display="flex" alignItems="baseline">
             { props.event.new === true && props.findEvents === true && (<Badge mr={1} borderRadius="full" px="2" colorScheme="teal">
               New
             </Badge>)}
-            { props.event.spotsRemaining <= 2 && (<Badge mr={1} borderRadius="full" px="2" colorScheme="green">
+            { props.event.ownership === true && (<Badge mr={1} borderRadius="full" px="2" colorScheme="green">
               Your Event
             </Badge>)}
-            {props.profileEvents ===  true && (<Badge mr={1} borderRadius="full" px="2" colorScheme="yellow">
-              Starts today
+            { props.event.spotsRemaining <= 2 && (<Badge mr={1} borderRadius="full" px="2" colorScheme="red">
+              Almost Full!
+            </Badge>)}
+            {props.event.startsToday === true && (<Badge mr={1} borderRadius="full" px="2" colorScheme="yellow">
+              Today
             </Badge>)}
 
+          </Box>
+        <Box  p="2">
+          <Heading fontWeight="semibold" fontSize={'sm'} >FRI APR 8, 8:00 PM</Heading>
             <Box
               color="gray.500"
               fontWeight="semibold"
               letterSpacing="wide"
               fontSize="xs"
               textTransform="uppercase"
-              ml="2"
             >
               {props.event.signedUp} signed up &bull;{" "}
               {props.event.spotsRemaining} spots remaining
             </Box>
-          </Box>
 
           <Box
-            mt="1"
+            mt="2"
             fontWeight="semibold"
+            fontSize={'xl'}
             as="h4"
             lineHeight="tight"
             isTruncated
           >
             {props.event.title}
           </Box>
-          <Box mt="1" as="h4" lineHeight="tight">
+          <Box fontSize={'md'} mb={2} ml={1} mr={3}  as="h4" lineHeight="tight">
             {props.event.description}
           </Box>
         </Box>
         {props.findEvents ===  true && (<Box display="flex" alignItems="center" justifyContent="flex-end">
+          
           <Button onClick={()=>{setShow(false)}} colorScheme="red" m={2} marginBottom="4">
             Swipe Left
           </Button>
@@ -78,7 +81,7 @@ function SportEvent(props) {
             Swipe Right
           </Button>
         </Box>)}
-        {props.profileEvents ===  true && (<Box display="flex" alignItems="center" justifyContent="flex-end">
+        {props.profileEvents ===  true && (<Box display="flex" alignItems="center" justifyContent="space-between">
           <Button onClick={()=>{}}  colorScheme="teal" m={2} marginBottom="4">
             View Event
           </Button>
@@ -86,7 +89,7 @@ function SportEvent(props) {
             Cancel Event
           </Button>
         </Box>)}
-
+        </Box>
       </Box>)}
     </>
   );
