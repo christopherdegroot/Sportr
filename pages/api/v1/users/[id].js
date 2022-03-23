b3d247911e35d5796c022a21e35983d79498cae9
Noah V <noah@noahvandenberg.com>
2022-03-23 14:39:06 -0700

    create users v1 api

---
+++ pages/api/v1/users/[id].js
@@ -0,0 +1,45 @@
+const db = require('../../../../db/db')
+
+export default async function handler(req, res) {
+  const { id } = req.query
+  res.status(200).json(req.query)
+  if (req.method === 'GET') {
+    try {
+      const query = {
+        text: 'SELECT * FROM users WHERE id = $1;',
+        values: [req.params.id],
+      };
+      const dbResponse = await db.query(query);
+      res.json(dbResponse.rows)
+    } catch (error) {
+      console.log(chalk.redBright('ERROR in users.js @ READ:', chalk.whiteBright(error)));
+    }
+  }
+  if (req.method === 'PUT') {
+    try {
+      // req.body object destructuring
+      const {email, name, password, bio, profile_image_url, gender, birth_date, sports, km_range} = req.body;
+      
+      const query = {
+        text: 'UPDATE users SET email=$1, name=$2, password=$3, bio=$4, profile_image_url=$5, gender=$6, birth_date=$7, sports=$8, km_range=$9 WHERE id=$10 RETURNING *;',
+        values: [email, name, password, bio, profile_image_url, gender, birth_date, sports, km_range, req.params.id],
+      };
+      const dbResponse = await db.query(query);
+      res.json(dbResponse.rows)
+    } catch (error) {
+      console.log(chalk.redBright('ERROR in users.js @ EDIT:', chalk.whiteBright(error)));
+    }
+  }
+  if (req.method === 'DELETE') {
+    try {
+      const query = {
+        text: 'DELETE FROM users WHERE id = $1 RETURNING *;',
+        values: [req.params.id],
+      };
+      const dbResponse = await db.query(query);
+      res.json(dbResponse.rows)
+    } catch (error) {
+      console.log(chalk.redBright('ERROR in users.js @ DELETE:', chalk.whiteBright(error)));
+    }
+  }
+}
--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -1,5 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    try {
+      const query = {
+        text: 'SELECT * FROM users;',
+      };
+      const dbResponse = await db.query(query);
+      res.json(dbResponse.rows)
+    } catch (error) {
+      console.log(chalk.redBright('ERROR in users.js @ BROWSE:', chalk.whiteBright(error)));
+    }
+  }
+  if (req.method === 'POST') {
+    try {
+      // req.body object destructuring
+      const {email, name, password, bio, profile_image_url, gender, birth_date, sports, km_range} = req.body;
+  
+      const query = {
+        text: 'INSERT INTO users (email, name, password, bio, profile_image_url, gender, birth_date, sports, km_range) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;',
+        values: [email, name, password, bio, profile_image_url, gender, birth_date, sports, km_range],
+      };
+      const dbResponse = await db.query(query);
+      res.json(dbResponse.rows)
+    } catch (error) {
+      console.log(chalk.redBright('ERROR in users.js @ ADD:', chalk.whiteBright(error)));
+    }
+  }
 }
