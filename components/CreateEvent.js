0acbb5b2f2b8ef9c260b2b745e7ded753397403b
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-22 17:32:03 -0700

    add react date-picker

--- components/CreateEvent.js
+++ components/CreateEvent.js
@@ -28,14 +28,17 @@ import {
   GridItem,
   CheckboxGroup,
   Grid,
-  Stack
+  Stack,
 } from "@chakra-ui/react";
 import Sportcheckbox from "./Sportcheckbox";
 import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Details = () => {
   const { isOpen, onToggle } = useDisclosure();
-  const [sliderValue, setSliderValue] = useState(50)
+  const [sliderValue, setSliderValue] = useState(50);
+  const [startDate, setStartDate] = useState(new Date());
 
   return (
     <Container maxW="container.md">
@@ -49,30 +52,39 @@ const Details = () => {
               <FormLabel>Event Name </FormLabel>
               <Input placeholder="First Name" />
             </FormControl>
+            <GridItem p={6} colSpan={2}>
+              <DatePicker
+                showTimeSelect
+                dateFormat="Pp"
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+              />
+            </GridItem>
           </GridItem>
           <GridItem colSpan={2}>
-          <FormControl>
+            <FormControl>
               <FormLabel>What sport would you like to play?</FormLabel>
               <Select placeholder="Sport">
-                <option value='spikeball'>Spike Ball</option>
-                <option value='discgolf'>Disc Golf</option>
-                <option value='basketball'>Basketball</option>
+                <option value="spikeball">Spike Ball</option>
+                <option value="discgolf">Disc Golf</option>
+                <option value="basketball">Basketball</option>
               </Select>
-
             </FormControl>
           </GridItem>
           <GridItem colSpan={2}>
             <FormControl>
               <FormLabel>Event Description</FormLabel>
-              <Textarea resize="none" h="7em" placeholder="Description"/>
+              <Textarea resize="none" h="7em" placeholder="Description" />
             </FormControl>
           </GridItem>
           <GridItem colSpan={2}>
-            <Text pb='3' fontSize={'lg'}>Any Preferences?</Text>
-          <Stack spacing={5} direction='row'>
-            <Checkbox>Same gender</Checkbox>
-            <Checkbox>Same age group</Checkbox>
-          </Stack>
+            <Text pb="3" fontSize={"lg"}>
+              Any Preferences?
+            </Text>
+            <Stack spacing={5} direction="row">
+              <Checkbox>Same gender</Checkbox>
+              <Checkbox>Same age group</Checkbox>
+            </Stack>
           </GridItem>
           <GridItem colSpan={2}>
             <Link href="/profile">
