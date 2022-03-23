d9da4d8a0841e2bc44fe4f8a7d6dfc0d7accf51f
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-22 17:28:06 -0700

    render create-event component

--- pages/create-event/index.js
+++ pages/create-event/index.js
@@ -2,21 +2,17 @@ import Link from 'next/link'
 import Head from 'next/head'
 import styles from '../../styles/login-header.module.css'
 import CreateEvent from '../../components/CreateEvent'
+import NavigationBar from '../../components/NavigationBar'
 
 import { Checkbox, Button, Select, FormControl, FormLabel, Input, VStack, Heading, Text, SimpleGrid, GridItem } from "@chakra-ui/react";
 
 const NewEvent = (props) => {
   return (
     <>
-    <VStack flexDirection='row' paddingBottom='3em'>
-      {!props.home && (
-        <div className={styles.backToHome}>
-         <Link href="/">
-            ← Back to home
-          </Link>
-        </div>
-      )}
-    </VStack>
+    <CreateEvent>
+    </CreateEvent>
+    
+    <NavigationBar/>
     </>
   )
   
