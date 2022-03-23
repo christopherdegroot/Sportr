dd884cba11ebb9ada4bcbf1e56dc320f9d1f02d8
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-22 20:58:16 -0700

    add top toggle switch for find/my events

--- pages/eventfeed/index.js
+++ pages/eventfeed/index.js
@@ -1,19 +1,39 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import { Box, Container, Flex, VStack } from '@chakra-ui/react'
+import { ButtonGroup, Button, Box, Container, Flex, VStack } from '@chakra-ui/react'
 import EventFeed from '../../components/EventFeed'
 import styles from '../../styles/login-header.module.css'
 import NavigationBar from '../../components/NavigationBar'
-
+import EventToggleButton from '../../components/EventToggleButton'
+import { useState } from 'react'
 
 export default function Userhome(props) {
+  const [leftToggle, setLeftToggle] = useState(true)
+  const swapToggle = function(tf) {
+    setLeftToggle(tf)
+  }
   return (
     <>
     <Container backgroundColor={'rgb(247, 247, 247)'} maxW="container.xl" p={0}>
-      <Flex  pb={20}>
-      <EventFeed findEvents={true}>
+      <Flex flexDirection={'column'} pb={20}>
 
-      </EventFeed>
+      <>
+          <VStack mt='10' flexDirection={'row'}  justifyContent={'center'}>
+          {leftToggle && (<ButtonGroup size='sm' isAttached variant='outline'>
+            <Button onClick={()=>{swapToggle(true)}} variant={'solid'} colorScheme={'teal'}  mr='-px'>Find Events</Button>
+            <Button onClick={()=>{swapToggle(false)}} variant={'outline'} colorScheme={'teal'} mr='-px'>My Events</Button>
+          </ButtonGroup>)}
+          {!leftToggle && (<ButtonGroup size='sm' isAttached variant='outline'>
+            <Button onClick={()=>{swapToggle(true)}} variant={'outline'} colorScheme={'teal'}  mr='-px'>Find Events</Button>
+            <Button onClick={()=>{swapToggle(false)}} variant={'solid'} colorScheme={'teal'} mr='-px'>My Events</Button>
+          </ButtonGroup>)}
+          </VStack>
+        </>
+        {leftToggle && (<EventFeed findEvents={true}>
+      </EventFeed>)}
+      {!leftToggle && (<EventFeed profileEvents={true}>
+      </EventFeed>)}
+
       </Flex>
     </Container>
     <NavigationBar/>
