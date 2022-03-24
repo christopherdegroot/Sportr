4ce1bff77771ee2ca63d020d5d1d584a93619741
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-23 23:59:27 -0700

    remove preferences from here

--- components/Register.js
+++ components/Register.js
@@ -29,27 +29,21 @@ import {
   CheckboxGroup,
   Grid,
   Stack,
-  useToast 
+  useToast,
 } from "@chakra-ui/react";
 import Sportcheckbox from "./Sportcheckbox";
 import { useState } from "react";
 
 const Details = (props) => {
   const { isOpen, onToggle } = useDisclosure();
-  const [sliderValue, setSliderValue] = useState(50)
-  const toast = useToast()
-
+  const [sliderValue, setSliderValue] = useState(50);
+  const toast = useToast();
 
   return (
     <Container maxW="container.md">
-      <VStack w="full" h="full" p={10} spacing={10} >
+      <VStack w="full" h="full" p={10} spacing={10}>
         <VStack spacing={3} alignItems="flex-start">
-          <Heading size="2xl"> Please Register </Heading>
-          <Text>
-            {" "}
-            If you already have an account, click <Link href="/">here</Link> to
-            log in{" "}
-          </Text>
+          <Heading size="2xl"> Register </Heading>
         </VStack>
         <SimpleGrid columns={2} rowGap={6} w="full">
           <GridItem colSpan={2}>
@@ -73,50 +67,73 @@ const Details = (props) => {
           <GridItem colSpan={2}>
             <FormControl>
               <FormLabel>Password </FormLabel>
-              <Input type="password" placeholder="Password"/>
+              <Input type="password" placeholder="Password" />
             </FormControl>
           </GridItem>
           <GridItem colSpan={2}>
             <FormControl>
               <FormLabel>Confirm Password </FormLabel>
-              <Input type="password" placeholder="Password"/>
+              <Input type="password" placeholder="Password" />
             </FormControl>
           </GridItem>
           <GridItem colSpan={2}>
             <FormControl>
               <FormLabel>Tell Us About Yourself </FormLabel>
-              <Textarea resize="none" h="7em" placeholder="Bio"/>
+              <Textarea resize="none" h="7em" placeholder="Bio" />
             </FormControl>
           </GridItem>
           <GridItem colSpan={2}>
-          <FormControl>
+            <FormControl>
               <FormLabel>What is your age?</FormLabel>
-              <Input type="number" placeholder="Age"/>
-
+              <Input type="number" placeholder="Age" />
             </FormControl>
           </GridItem>
           <GridItem colSpan={2}>
-          <FormControl>
+            <FormControl>
               <FormLabel>What is your gender?</FormLabel>
               <Select placeholder="Gender">
-                <option value='male'>Male</option>
-                <option value='female'>Female</option>
-                <option value='male'>Other</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="male">Other</option>
               </Select>
             </FormControl>
           </GridItem>
-          
+
+          <GridItem colSpan={2}>
+            <FormLabel pb={5}>Select Range</FormLabel>
+            <Slider
+              aria-label="slider-ex-6"
+              onChange={(val) => setSliderValue(val)}
+            >
+              <SliderMark
+                rounded={"md"}
+                value={sliderValue}
+                textAlign="center"
+                bg="teal.500"
+                color="white"
+                mt="-10"
+                ml="-7"
+                w="14"
+              >
+                {sliderValue}km
+              </SliderMark>
+              <SliderTrack>
+                <SliderFilledTrack bg="teal.500" />
+              </SliderTrack>
+              <SliderThumb />
+            </Slider>
+          </GridItem>
           <GridItem colSpan={2}>
             {/* checkbox component has an on user changed event that we can use to reflect on the users choice */}
-            <Button w={'100%'} colorScheme={'teal'} onClick={onToggle}>
+            <Button w={"100%"} colorScheme={"teal"} onClick={onToggle}>
               What sports interest you?
             </Button>
             <Collapse colSpan={2} in={isOpen} animateOpacity>
               <Box
                 p="40px"
-                color="white"
+                color="black"
                 mt="4"
-                bg="teal.500"
+                bg="rgb(247, 247, 247)"
                 rounded="md"
                 shadow="md"
               >
@@ -124,53 +141,31 @@ const Details = (props) => {
               </Box>
             </Collapse>
           </GridItem>
-          <GridItem colSpan={2}>
-          </GridItem>
-          <GridItem colSpan={2}>
-            <FormLabel pb={5} >Select Range</FormLabel>
-            <Slider aria-label='slider-ex-6' onChange={(val) => setSliderValue(val)}>
-              <SliderMark
-                rounded={'md'} 
-                value={sliderValue}
-                textAlign='center'
-                bg='teal.500'
-                color='white'
-                mt='-10'
-                ml='-7'
-                w='14'
-              >
-                {sliderValue}km
-              </SliderMark>
-              <SliderTrack>
-                <SliderFilledTrack bg='teal.500' />
-              </SliderTrack>
-              <SliderThumb />
-            </Slider>
-          </GridItem>
-          <GridItem></GridItem>
-          <GridItem colSpan={2}>
-            <Text pb='3' fontSize={'lg'}>Any Preferences?</Text>
-          <Stack spacing={5} direction='row'>
-            <Checkbox>Events with my gender</Checkbox>
-            <Checkbox>Events with my age group</Checkbox>
-          </Stack>
-          </GridItem>
-          {props.registerPage && (<GridItem colSpan={2}>
-            <Link href="/profile">
-              <Button colorScheme="teal" w="full" size="lg" onClick={() =>
-                toast({
-                  title: 'Account created.',
-                  description: "Your account has been successfully created.",
-                  status: 'success',
-                  duration: 3000,
-                  isClosable: true,
-                })}
->
-                Sign up
-              </Button>
-              
-            </Link>
-          </GridItem>)}
+          <GridItem colSpan={2}></GridItem>
+         
+          {props.registerPage && (
+            <GridItem colSpan={2}>
+              <Link href="/profile">
+                <Button
+                  colorScheme="teal"
+                  w="full"
+                  size="lg"
+                  onClick={() =>
+                    toast({
+                      title: "Account created.",
+                      description:
+                        "Your account has been successfully created.",
+                      status: "success",
+                      duration: 3000,
+                      isClosable: true,
+                    })
+                  }
+                >
+                  Sign up
+                </Button>
+              </Link>
+            </GridItem>
+          )}
         </SimpleGrid>
       </VStack>
     </Container>
