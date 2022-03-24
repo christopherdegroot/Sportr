c5c1645d4d7428b0e5ed1f9bb67aca75cf387967
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-23 19:08:35 -0700

    add settings for carousel

--- components/carousel/ImageSlider.js
+++ components/carousel/ImageSlider.js
@@ -5,7 +5,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 // If you want to use your own Selectors look up the Advancaed Story book examples
 const ImageSlider = ({ slides }) => {
   return (
-    <Carousel showIndicators={false} showStatus={false} infiniteLoop>
+    <Carousel showThumbs={false} preventMovementUntilSwipeScrollTolerance={true} autoPlay={false} dynamicHeight={true} showIndicators={false} showStatus={false} infiniteLoop>
       {slides.map((slide) => {
         return <Image src={slide.image} height="auto" width="800px" />;
       })}
