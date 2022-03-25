ac6b9dc0382f1db899c747a0c4ed3d1451a8fe19
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-24 19:11:05 -0700

    add image for each sport  type

--- components/carousel/SlideData.js
+++ components/carousel/SlideData.js
@@ -1,22 +1,121 @@
+
 export const SlideData = [
   {
+    key: "badminton",
     image:
-      "https://crs4rec.com/wp-content/uploads/2018/09/moody-park-gallery-014.jpg?x65263"
+      "https://i.imgur.com/GYUWJGb.jpg",
   },
   {
+    key: "baseball",
     image:
-      "https://i.imgur.com/PZ59UV5.png"
+      "https://i.imgur.com/Z4cXLIo.jpg"
   },
   {
-    image:
-      "https://604now.com/wp-content/uploads/2015/06/english-bay-beach-1280x720.jpg"
+    key: "basketball",
+    image: "https://i.imgur.com/1XCQOiD.jpg"
   },
   {
-    image:
-      "https://udisc-parse.s3.amazonaws.com/765a22b7e3e35fe42ce428ebac2ad1d7_m_4tEhdmmTDdf2p6rPXoadoh3zrqrC.jpg"
+    key: "beachvolleyball",
+    image: "https://i.imgur.com/ZII0KKz.jpg"
   },
   {
-    image:
-      "https://runvan.org/wp-content/uploads/2020/07/41-2019-BMOVancouverHalfMarathon-115-41-HalfShirts-MayliesLang-RUNVAN%C2%AE.jpg"
-  }
+    key: "curling",
+    image: "https://i.imgur.com/5VW4y12.jpg"
+  },
+  {
+    key: "cycling",
+    image: "https://i.imgur.com/TMpqW6g.jpg"
+  },
+  {
+    key: "discgolf",
+    image: "https://i.imgur.com/Xj0iLJ4.jpg"
+  },
+  {
+    key: "football",
+    image: "https://i.imgur.com/E2y7Q3P.jpg"
+  },
+  {
+    key: "golf",
+    image: "https://i.imgur.com/yIcDokm.jpg"
+  },
+  {
+    key: "hiking",
+    image: "https://i.imgur.com/7cHYjrI.jpg"
+  },
+  {
+    key: "iceclimbing",
+    image: "https://i.imgur.com/2EDOBWg.jpg"
+  },
+  {
+    key: "kayaking",
+    image: "https://i.imgur.com/4jjeLI7.jpg"
+  },
+  {
+    key: "lacrosse",
+    image: "https://i.imgur.com/l3JfMwF.jpg"
+  },
+  {
+    key: "pickleball",
+    image: "https://i.imgur.com/QvlKNBy.jpg"
+  },
+  {
+    key: "racquetball",
+    image: "https://i.imgur.com/NCUluyX.jpg"
+  },
+  {
+    key: "rockclimbing",
+    image: "https://i.imgur.com/pcN4pFV.jpg"
+  },
+  {
+    key: "rowing",
+    image: "https://i.imgur.com/ZcfG8ip.jpg"
+  },
+  {
+    key: "rugby",
+    image: "https://i.imgur.com/KDeZo1w.jpg"
+  },
+  {
+    key: "running",
+    image: "https://i.imgur.com/nv9SxES.jpg"
+  },
+  {
+    key: "sailing",
+    image: "https://i.imgur.com/13aoDxn.jpg"
+  },
+  {
+    key: "soccer",
+    image: "https://i.imgur.com/MQ204wW.jpg"
+  },
+  {
+    key: "spikeball",
+    image: "https://i.imgur.com/pjBwOqt.jpg"
+  },
+  {
+    key: "squash",
+    image: "https://i.imgur.com/afRLUdQ.jpg"
+  },
+  {
+    key: "tabletennis",
+    image: "https://i.imgur.com/OKjC75z.jpg"
+  },
+  {
+    key: "tennis",
+    image: "https://i.imgur.com/kkZr2Ii.jpg"
+  },
+  {
+    key: "trailrunning",
+    image: "https://i.imgur.com/fTKejGU.jpg"
+  },
+  {
+    key: "ultimatefrisbee",
+    image: "https://i.imgur.com/OFJ7Vyh.jpg"
+  },
+  {
+    key: "volleyball",
+    image: "https://i.imgur.com/6IY0loX.jpg"
+  },
+  {
+    key: "yoga",
+    image: "https://i.imgur.com/QJsI8As.jpg"
+  },
 ];
