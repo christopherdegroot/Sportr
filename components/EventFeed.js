8637ecc7aff495a878ede1ddca7f8cda8d6b40f9
Noah V <noah@noahvandenberg.com>
2022-03-27 13:40:38 -0700

    remove draft events from eventfeed

--- components/EventFeed.js
+++ components/EventFeed.js
@@ -14,63 +14,6 @@ import useApplicationData from "../hooks/useApplicationData";
 import { getEventsForUser } from "../helpers/selectors";
 
 export default function EventFeed(props) {
-  let date = new Date(Date.now())
-
-  const event1 = {
-    imageUrl: "https://i.imgur.com/PZ59UV5.png",
-    signedUp: 3,
-    spotsRemaining: 2,
-    title: "Spikeball at Jericho",
-    description:
-      "Summer is upon us! Let's get out there and play some spike ball people, looking for chill vibes, bring your friends.",
-    new: true,
-    date: date
-  };
-
-  const event2 = {
-    imageUrl:
-      "https://crs4rec.com/wp-content/uploads/2018/09/moody-park-gallery-014.jpg?x65263",
-    signedUp: 6,
-    spotsRemaining: 2,
-    title: "Basketball at Moody Park",
-    description: "Looking for more for a pickup game at the Moody Park courts",
-    new: false,
-    startsToday: true
-  };
-
-  const event3 = {
-    imageUrl:
-      "https://604now.com/wp-content/uploads/2015/06/english-bay-beach-1280x720.jpg",
-    signedUp: 1,
-    spotsRemaining: 7,
-    title: "Attention beach volleyballers",
-    description: "I'll bring the net and balls, you bring the stoke!",
-    new: false,
-    ownership: true,
-  };
-
-  const event4 = {
-    imageUrl:
-      "https://udisc-parse.s3.amazonaws.com/765a22b7e3e35fe42ce428ebac2ad1d7_m_4tEhdmmTDdf2p6rPXoadoh3zrqrC.jpg",
-    signedUp: 1,
-    spotsRemaining: 2,
-    title: "Disc Golf at Raptor's Knoll",
-    description: "Looking for 3 more to fill out the card",
-    new: false,
-    ownership: false,
-  };
-
-  const event5 = {
-    imageUrl:
-      "https://runvan.org/wp-content/uploads/2020/07/41-2019-BMOVancouverHalfMarathon-115-41-HalfShirts-MayliesLang-RUNVAN%C2%AE.jpg",
-    signedUp: 2,
-    spotsRemaining: 2,
-    title: "Girls running club",
-    description: "Looking for some girls to form a weekly running crew in Vancouver",
-    new: false,
-    ownership: true,
-    date: Date.now()
-  };
 
   const { state } = useApplicationData()
   const myEvents = getEventsForUser(state, 4) // User ID as seconds arg
