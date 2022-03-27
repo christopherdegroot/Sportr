b7ebb542037b30bdbb5dc7524d7d99afd86ef0d9
Noah V <noah@noahvandenberg.com>
2022-03-27 14:48:58 -0700

    create a selector to count the users signed up for an event

--- helpers/selectors.js
+++ helpers/selectors.js
@@ -23,7 +23,24 @@ export function getUsersForEvent(state, event_id) {
   const userIds = eventRelations.map(relationObj => relationObj.user_id);
 
   // Grab all events that match event ids in eventIds
-  const eventsUsers = events.filter(users => usersIds.includes(user.id))
+  const eventsUsers = users.filter(user => userIds.includes(user.id))
 
   return eventsUsers
+}
+
+export function getSignedUpUserCountForEvent(state, event_id) {
+  const { events, users, users_events } = state;
+
+  // Get all users_events pertaining to the event_id
+  const eventRelations = users_events.filter(relationObj => relationObj.event_id === event_id);
+
+  // Grab all the event ids from those user events
+  const userIds = eventRelations.map(relationObj => relationObj.user_id);
+
+  // Grab all events that match event ids in eventIds
+  const eventsUsers = users.filter(user => userIds.includes(user.id))
+
+  const count = eventsUsers.length
+
+  return count
 }
