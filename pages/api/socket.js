bd319b3e60dbe1c85d32a124d0296c49b6225cb4
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-28 16:24:35 -0700

    add socket.js file

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,8 +1,10 @@
 import { useEffect } from 'react'
 const io = require("socket.io-client");
 import { Server } from 'socket.io'
+export const sockets = [];
 
 const SocketHandler = (req, res) => {
+
   if (res.socket.server.io) {
     console.log('Socket is already running')
   } else {
@@ -10,6 +12,7 @@ const SocketHandler = (req, res) => {
     const io = new Server(res.socket.server)
     res.socket.server.io = io
     io.on('connection', socket => {
+      sockets.push(socket)
       // I think we can add more .on handlers here to watch for changes to things and update with emits
       socket.on('input-change', msg => {
         socket.broadcast.emit('update-input', msg)
