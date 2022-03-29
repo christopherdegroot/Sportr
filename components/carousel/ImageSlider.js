bb22f3c2cd783532dcfd21d3afebd2d817693b63
Noah V <noah@noahvandenberg.com>
2022-03-29 09:19:32 -0700

    change sport event image carosel to use next/image

--- components/carousel/ImageSlider.js
+++ components/carousel/ImageSlider.js
@@ -1,13 +1,17 @@
-import { Image } from "@chakra-ui/react";
+import Image from "next/image";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const imageLoader = ({ src, width, quality }) => {
+  return `${src}?w=${width}&q=${quality || 75}`
+}
 
 // If you want to use your own Selectors look up the Advancaed Story book examples
 const ImageSlider = ({ slides }) => {
   return (
     <Carousel showThumbs={false} preventMovementUntilSwipeScrollTolerance={true} autoPlay={false} dynamicHeight={true} showIndicators={false} showStatus={false} infiniteLoop>
       {slides.map((slide) => {
-        return <Image src={slide.image} key={slide.key} height="auto" width="800px" />;
+        return <Image loader={imageLoader} src={slide.image} key={slide.key} height="400px" width="650px" alt={`${slide.sport} image`} />;
       })}
     </Carousel>
   );
