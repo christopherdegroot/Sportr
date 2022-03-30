73c49a59e203e46eb3d0386083fe7c5efcb2e3be
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-03-30 10:46:18 -0700

    update keys for carousel data

--- components/carousel/SlideData.js
+++ components/carousel/SlideData.js
@@ -1,187 +1,187 @@
 
 export const SlideData = [
   {
-    key: "Badminton",
+    key: "badminton",
     image: "https://i.imgur.com/GYUWJGb.jpg",
     lat: 49.258305, 
     long: -123.142757
   },
   {
-    key: "Baseball",
+    key: "baseball",
     image: "https://i.imgur.com/Z4cXLIo.jpg",
     lat: 49.224926, 
     long: -123.160742
   },
   {
-    key: "Basketball",
+    key: "basketball",
     image: "https://i.imgur.com/1XCQOiD.jpg",
     lat: 49.275783, 
     long: -123.152228
   },
   {
-    key: "Beach Volleyball",
+    key: "beach volleyball",
     image: "https://i.imgur.com/ZII0KKz.jpg",
     lat: 49.272202, 
     long: -123.192684
   },
   {
-    key: "Curling",
+    key: "curling",
     image: "https://i.imgur.com/5VW4y12.jpg",
     lat: 49.244466, 
     long: -123.107195
   },
   {
-    key: "Cycling",
+    key: "cycling",
     image: "https://i.imgur.com/TMpqW6g.jpg",
     lat: 49.297425, 
     long: -123.132427
   },
   {
-    key: "Disc Golf",
+    key: "disc golf",
     image: "https://i.imgur.com/Xj0iLJ4.jpg",
     lat: 49.239484,
     long:  -123.106320
   },
   {
-    key: "Football",
+    key: "football",
     image: "https://i.imgur.com/E2y7Q3P.jpg",
     lat: 49.230141, 
     long: -123.087044
   },
   {
-    key: "Golf",
+    key: "golf",
     image: "https://i.imgur.com/yIcDokm.jpg",
     lat: 49.233144, 
     long: -123.209523
   },
   {
-    key: "Hiking",
+    key: "hiking",
     image: "https://i.imgur.com/7cHYjrI.jpg",
     lat: 49.371385,
     long: -123.098563
   },
   {
-    key: "Ice Climbing",
+    key: "ice climbing",
     image: "https://i.imgur.com/2EDOBWg.jpg",
     lat: 52.212498, 
     long: -117.232816
   },
   {
-    key: "Kayaking",
+    key: "kayaking",
     image: "https://i.imgur.com/4jjeLI7.jpg",
     lat: 49.079890, 
     long: -121.794851
   },
   {
-    key: "Lacrosse",
+    key: "lacrosse",
     image: "https://i.imgur.com/l3JfMwF.jpg",
     lat: 49.215433, 
     long: -122.904667
   },
   {
-    key: "Pickleball",
+    key: "pickleball",
     image: "https://i.imgur.com/QvlKNBy.jpg",
     lat: 49.258305, 
     long: -123.142757
   },
   {
-    key: "Racquetball",
+    key: "racquetball",
     image: "https://i.imgur.com/NCUluyX.jpg",
     lat: 49.258305, 
     long: -123.142757
   },
   {
-    key: "Rock Climbing",
+    key: "rock climbing",
     image: "https://i.imgur.com/pcN4pFV.jpg",
     lat: 49.682447,
     long:  -123.148072
   },
   {
-    key: "Rowing",
+    key: "rowing",
     image: "https://i.imgur.com/ZcfG8ip.jpg",
     lat: 49.245290,
     long: -122.959242
   },
   {
-    key: "Rugby",
+    key: "rugby",
     image: "https://i.imgur.com/KDeZo1w.jpg",
     lat: 49.230141, 
     long: -123.087044
   },
   {
-    key: "Running",
+    key: "running",
     image: "https://i.imgur.com/nv9SxES.jpg",
     lat: 49.297425, 
     long: -123.132427
   },
   {
-    key: "Sailing",
+    key: "sailing",
     image: "https://i.imgur.com/13aoDxn.jpg",
     lat: 48.891814,
     long: -123.391162
   },
   {
-    key: "Skiing",
+    key: "skiing",
     image: "https://i.imgur.com/PGWfrAy.jpg",
     lat: 50.113096,
     long: -122.953630
   },
   {
-    key: "Soccer",
+    key: "soccer",
     image: "https://i.imgur.com/PGWfrAy.jpg",
     lat: 49.259865,
     long:  -123.050459
   },
   {
-    key: "Spike Ball",
+    key: "spike ball",
     image: "https://i.imgur.com/pjBwOqt.jpg",
     lat: 49.276328,
     long:  -123.212463
   },
   {
-    key: "Squash",
+    key: "squash",
     image: "https://i.imgur.com/afRLUdQ.jpg",
     lat: 49.258305, 
     long: -123.142757
   },
   {
-    key: "Swimming",
+    key: "swimming",
     image: "https://i.imgur.com/AInP5zg.jpeg",
     lat: 49.276328,
     long: -123.212463
   },
   {
-    key: "Table Tennis",
+    key: "table tennis",
     image: "https://i.imgur.com/OKjC75z.jpg",
     lat: 49.258305, 
     long: -123.142757
   },
   {
-    key: "Tennis",
+    key: "tennis",
     image: "https://i.imgur.com/kkZr2Ii.jpg",
     lat: 49.258305, 
     long: -123.142757
   },
   {
-    key: "Trail Running",
+    key: "trail running",
     image: "https://i.imgur.com/fTKejGU.jpg",
     lat: 49.366963,
     long:  -123.275191
   },
   {
-    key: "Ultimate Frisbee",
+    key: "ultimate frisbee",
     image: "https://i.imgur.com/OFJ7Vyh.jpg",
     lat: 49.259865,
     long:  -123.050459
   },
   {
-    key: "Volleyball",
+    key: "volleyball",
     image: "https://i.imgur.com/6IY0loX.jpg",
     lat: 49.230797,
     long:  -123.019957
   },
   {
-    key: "Yoga",
+    key: "yoga",
     image: "https://i.imgur.com/QJsI8As.jpg",
     lat: 49.277607,
     long:  -123.148952
