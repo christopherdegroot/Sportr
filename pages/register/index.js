d5d0618c6c38fb3c43e1ea94685cacf828ad20db
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-04-07 11:34:55 -0700

    prettier

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,22 +1,29 @@
-import Link from 'next/link'
-import Head from 'next/head'
-import styles from '../../styles/login-header.module.css'
-import Register from '../../components/Register'
+import Link from "next/link";
+import Head from "next/head";
+import styles from "../../styles/login-header.module.css";
+import Register from "../../components/Register";
 
-import { Checkbox, Button, Select, FormControl, FormLabel, Input, VStack, Heading, Text, SimpleGrid, GridItem } from "@chakra-ui/react";
+import {
+  Checkbox,
+  Button,
+  Select,
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+  Heading,
+  Text,
+  SimpleGrid,
+  GridItem,
+} from "@chakra-ui/react";
 
 const Login = (props) => {
   return (
     <>
-    <Register registerPage={true}>
-      
-    </Register>
-    <VStack flexDirection='row' paddingBottom='3em'>
-     
-    </VStack>
+      <Register registerPage={true}></Register>
+      <VStack flexDirection="row" paddingBottom="3em"></VStack>
     </>
-  )
-  
-}
+  );
+};
 
 export default Login;
