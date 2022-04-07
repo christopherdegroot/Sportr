01e1fe73abff452236ce3486fedce265ef65b5f9
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-04-07 11:37:39 -0700

    eslint, prettier

--- pages/create-event/index.js
+++ pages/create-event/index.js
@@ -1,23 +1,21 @@
-import Link from 'next/link'
-import Head from 'next/head'
-import styles from '../../styles/login-header.module.css'
-import CreateEvent from '../../components/CreateEvent'
-import NavigationBar from '../../components/NavigationBar'
-import { getCookie } from 'cookies-next';
-
-import { Checkbox, Button, Select, FormControl, FormLabel, Input, VStack, Heading, Text, SimpleGrid, GridItem } from "@chakra-ui/react";
+import CreateEvent from "../../components/CreateEvent";
+import NavigationBar from "../../components/NavigationBar";
+import { getCookie } from "cookies-next";
 import useApplicationData from "../../hooks/useApplicationData";
+
 const NewEvent = (props) => {
   const { state, createEvent } = useApplicationData();
   return (
     <>
-    <CreateEvent state={state} createEvent={createEvent} user_id={getCookie('user_id')}>
-    </CreateEvent>
-    
-    <NavigationBar/>
+      <CreateEvent
+        state={state}
+        createEvent={createEvent}
+        user_id={getCookie("user_id")}
+      ></CreateEvent>
+
+      <NavigationBar />
     </>
-  )
-  
-}
+  );
+};
 
 export default NewEvent;
