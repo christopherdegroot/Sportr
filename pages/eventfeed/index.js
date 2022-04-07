f98307c3bd027eb60bb1e36d95394e55cb184e49
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-04-07 11:38:06 -0700

    prettier

--- pages/eventfeed/index.js
+++ pages/eventfeed/index.js
@@ -28,81 +28,88 @@ export default function Userhome(props) {
 
   return (
     <>
-        <VStack pt={7}>
-          {leftToggle && (
-            <ButtonGroup size="sm" isAttached variant="outline">
-              <Button
-                size={"md"}
-                onClick={() => {
-                  swapToggle(true);
-                }}
-                variant={"solid"}
-                colorScheme={"teal"}
-                mr="-px"
-              >
-                Find Events
-              </Button>
-              <Button
-                size={"md"}
-                onClick={() => {
-                  swapToggle(false);
-                }}
-                variant={"outline"}
-                colorScheme={"teal"}
-                mr="-px"
-              >
-                My Events
-              </Button>
-            </ButtonGroup>
-          )}
-          {!leftToggle && (
-            <ButtonGroup size="sm" isAttached variant="outline">
-              <Button
-                size={"md"}
-                onClick={() => {
-                  swapToggle(true);
-                }}
-                variant={"outline"}
-                colorScheme={"teal"}
-                mr="-px"
-              >
-                Find Events
-              </Button>
-              <Button
-                size={"md"}
-                onClick={() => {
-                  swapToggle(false);
-                }}
-                variant={"solid"}
-                colorScheme={"teal"}
-                mr="-px"
-              >
-                My Events
-              </Button>
-            </ButtonGroup>
-          )}
-        </VStack>
+      <VStack pt={7}>
+        {leftToggle && (
+          <ButtonGroup size="sm" isAttached variant="outline">
+            <Button
+              size={"md"}
+              onClick={() => {
+                swapToggle(true);
+              }}
+              variant={"solid"}
+              colorScheme={"teal"}
+              mr="-px"
+            >
+              Find Events
+            </Button>
+            <Button
+              size={"md"}
+              onClick={() => {
+                swapToggle(false);
+              }}
+              variant={"outline"}
+              colorScheme={"teal"}
+              mr="-px"
+            >
+              My Events
+            </Button>
+          </ButtonGroup>
+        )}
+        {!leftToggle && (
+          <ButtonGroup size="sm" isAttached variant="outline">
+            <Button
+              size={"md"}
+              onClick={() => {
+                swapToggle(true);
+              }}
+              variant={"outline"}
+              colorScheme={"teal"}
+              mr="-px"
+            >
+              Find Events
+            </Button>
+            <Button
+              size={"md"}
+              onClick={() => {
+                swapToggle(false);
+              }}
+              variant={"solid"}
+              colorScheme={"teal"}
+              mr="-px"
+            >
+              My Events
+            </Button>
+          </ButtonGroup>
+        )}
+      </VStack>
 
-        <Stack flexDirection={'column-reverse'} alignItems={'center'} justifyContent={'center'} alignContent={"center"} p={3} pb={20}>
-          {leftToggle && (
-            <EventFeed
-              user_id={getCookie("user_id")}
-              state={state}
-              createUserEvent={createUserEvent}
-              changeUserEvent={changeUserEvent}
-              findEvents={true}
-            ></EventFeed>
-          )}
-          {!leftToggle && (
-            <EventFeed
-              user_id={getCookie("user_id")}
-              state={state}
-              createUserEvent={createUserEvent}
-              changeUserEvent={changeUserEvent}
-              profileEvents={true}
-            ></EventFeed>
-          )}
-        </Stack>
+      <Stack
+        flexDirection={"column-reverse"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        alignContent={"center"}
+        p={3}
+        pb={20}
+      >
+        {leftToggle && (
+          <EventFeed
+            user_id={getCookie("user_id")}
+            state={state}
+            createUserEvent={createUserEvent}
+            changeUserEvent={changeUserEvent}
+            findEvents={true}
+          ></EventFeed>
+        )}
+        {!leftToggle && (
+          <EventFeed
+            user_id={getCookie("user_id")}
+            state={state}
+            createUserEvent={createUserEvent}
+            changeUserEvent={changeUserEvent}
+            profileEvents={true}
+          ></EventFeed>
+        )}
+      </Stack>
       <NavigationBar />
     </>
   );
