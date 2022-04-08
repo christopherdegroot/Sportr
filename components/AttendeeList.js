6723dc002553505cf3e7a06be781c901f9dc07a1
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-04-08 14:59:32 -0700

    prettier and eslint

--- components/AttendeeList.js
+++ components/AttendeeList.js
@@ -1,20 +1,20 @@
-import { chakra } from "@chakra-ui/react";
 import React from "react";
 
 import InterviewerListItem from "./AttendeeListItem";
-import {Box, Divider, Text} from "@chakra-ui/react";
+import { Box, Divider, Text } from "@chakra-ui/react";
 
 import { getUsersForEvent } from "../helpers/selectors";
 
 // InterviewerList component
-function AttendeeList(props) {
+const AttendeeList = (props) => {
+  const { state } = props;
 
-  const { state } = props
+  const ownerid = props.event.user_owner;
+  const owner = state.users.find((user) => user.id === ownerid); // array not object
 
-  const ownerid = props.event.user_owner
-  const owner = state.users.find( user => user.id === ownerid) // array not object
-
-  const attendees = getUsersForEvent(state, props.event.id).filter( attendee => attendee.id !== ownerid)
+  const attendees = getUsersForEvent(state, props.event.id).filter(
+    (attendee) => attendee.id !== ownerid
+  );
 
   const parsedAttendeeList = attendees.map((attendee) => (
     <InterviewerListItem
@@ -26,14 +26,14 @@ function AttendeeList(props) {
 
   return (
     <Box>
-      <InterviewerListItem
-      {...owner}
-      />
-      <Divider height={3} ></Divider> 
-      <Text fontWeight={'hairline'} pt={2} fontSize={20}>Attendees:</Text>  
+      <InterviewerListItem {...owner} />
+      <Divider height={3}></Divider>
+      <Text fontWeight={"hairline"} pt={2} fontSize={20}>
+        Attendees:
+      </Text>
       {parsedAttendeeList}
     </Box>
   );
-}
+};
 
 export default AttendeeList;
