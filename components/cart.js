f340dade6f0b642e1b2046d597373618de9449ef
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-04-12 13:25:36 -0700

    prettier

--- components/cart.js
+++ components/cart.js
@@ -4,20 +4,10 @@ import { VStack, Heading } from "@chakra-ui/react";
 
 const Cart = () => {
   return (
-    <VStack 
-      w="full" 
-      h="full" 
-      p={10} 
-      spacing={10} 
-      alignItems="center" 
-    >
-      <Heading>
-        Event Feed
-      </Heading>
-      
-      
+    <VStack w="full" h="full" p={10} spacing={10} alignItems="center">
+      <Heading>Event Feed</Heading>
     </VStack>
-  )
-}
+  );
+};
 
 export default Cart;
