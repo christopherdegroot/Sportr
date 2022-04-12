4b2e68f87dbf38f6ee12247289651393ebfaa054
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-04-12 13:26:24 -0700

    prettier

--- components/details.js
+++ components/details.js
@@ -1,24 +1,32 @@
 // test doc for chakra
 
-import { Checkbox, Button, Select, FormControl, FormLabel, Input, VStack, Heading, Text, SimpleGrid, GridItem } from "@chakra-ui/react";
+import {
+  Checkbox,
+  Button,
+  Select,
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+  Heading,
+  Text,
+  SimpleGrid,
+  GridItem,
+} from "@chakra-ui/react";
 
 const Details = () => {
   return (
-    <VStack 
-      w="full" 
-      h="full" 
-      p={10} 
-      spacing={10} 
-      alignItems="center" 
+    <VStack
+      w="full"
+      h="full"
+      p={10}
+      spacing={10}
+      alignItems="center"
       bg="gray.50"
     >
-      <Heading>
-        My Events
-      </Heading>
-      
-      
+      <Heading>My Events</Heading>
     </VStack>
-  )
-}
+  );
+};
 
 export default Details;
