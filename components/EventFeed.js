9d01697e02718994033d6778646c2f70a3c199ed
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-04-13 18:52:36 -0700

    prettier

--- components/EventFeed.js
+++ components/EventFeed.js
@@ -6,42 +6,59 @@ import {
   VStack,
   Heading,
   Stack,
-  Button
+  Button,
 } from "@chakra-ui/react";
 import SportEvent from "./SportEvent";
 import styles from "./layout.module.css";
-import Link from 'next/link';
+import Link from "next/link";
 
-import { getEventsForUser, getPotentialEventsForUser } from "../helpers/selectors";
-
+import {
+  getEventsForUser,
+  getPotentialEventsForUser,
+} from "../helpers/selectors";
 
 export default function EventFeed(props) {
-  const { state, createUserEvent, changeUserEvent } = props
-  const myEvents = getEventsForUser(state, Number(props.user_id)) // User ID as seconds arg
-  const potentialEvents = getPotentialEventsForUser(state, Number(props.user_id))
-  console.log('PING', myEvents, potentialEvents)
+  const { state, createUserEvent, changeUserEvent } = props;
+  const myEvents = getEventsForUser(state, Number(props.user_id)); // User ID as seconds arg
+  const potentialEvents = getPotentialEventsForUser(
+    state,
+    Number(props.user_id)
+  );
+  console.log("PING", myEvents, potentialEvents);
 
-  const myEventsReact = myEvents.map(event => (
+  const myEventsReact = myEvents.map((event) => (
     <Box py={2} key={event.id}>
-      <SportEvent state={state} user_id={props.user_id} createUserEvent={createUserEvent} changeUserEvent={changeUserEvent} onClose={onClose} event={event} profileEvents={true}/>
+      <SportEvent
+        state={state}
+        user_id={props.user_id}
+        createUserEvent={createUserEvent}
+        changeUserEvent={changeUserEvent}
+        onClose={onClose}
+        event={event}
+        profileEvents={true}
+      />
     </Box>
   ));
-  const allEventsReact = potentialEvents.map(event => (
-    <Box  py={2} key={event.id}>
-      <SportEvent state={state} user_id={props.user_id} createUserEvent={createUserEvent} changeUserEvent={changeUserEvent} onClose={onClose} event={event} findEvents={true}/>
+  const allEventsReact = potentialEvents.map((event) => (
+    <Box py={2} key={event.id}>
+      <SportEvent
+        state={state}
+        user_id={props.user_id}
+        createUserEvent={createUserEvent}
+        changeUserEvent={changeUserEvent}
+        onClose={onClose}
+        event={event}
+        findEvents={true}
+      />
     </Box>
   ));
 
   const { isOpen, onClose, onToggle } = useDisclosure();
 
   return (
-        <>
-            {props.findEvents && (<>
-              {allEventsReact}
-            </>)}
-            {props.profileEvents && (<>
-              {myEventsReact}
-            </>)}
-            </>
+    <>
+      {props.findEvents && <>{allEventsReact}</>}
+      {props.profileEvents && <>{myEventsReact}</>}
+    </>
   );
 }
