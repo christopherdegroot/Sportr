ab5049a0528f0ff8afa05752adf4c93c6d1379de
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-04-15 00:14:06 -0700

    prettier

--- components/RangeSlider.js
+++ components/RangeSlider.js
@@ -8,29 +8,30 @@ import {
 import { useState } from "react";
 
 const RangeSlider = (props) => {
-
-  const {defaultValue, onChangeEnd} = props
-  const [sliderValue, setSliderValue] = useState(defaultValue)
+  const { defaultValue, onChangeEnd } = props;
+  const [sliderValue, setSliderValue] = useState(defaultValue);
 
   return (
-    <Slider aria-label='slider-ex-6' defaultValue={sliderValue} onChange={(val) => setSliderValue(val)} onChangeEnd={(val) => console.log('Update KM Range', val)} >
-    <SliderMark value={25} mt='2' ml='-2.5' fontSize='sm'>
-      25km
-    </SliderMark>
-    <SliderMark value={50} mt='2' ml='-2.5' fontSize='sm'>
-      50km
-    </SliderMark>
-    <SliderMark value={75} mt='2' ml='-2.5' fontSize='sm'>
-      75km
-    </SliderMark>
+    <Slider
+      aria-label="slider-ex-6"
+      defaultValue={sliderValue}
+      onChange={(val) => setSliderValue(val)}
+      onChangeEnd={(val) => console.log("Update KM Range", val)}
+    >
+      <SliderMark value={25} mt="2" ml="-2.5" fontSize="sm">
+        25km
+      </SliderMark>
+      <SliderMark value={50} mt="2" ml="-2.5" fontSize="sm">
+        50km
+      </SliderMark>
+      <SliderMark value={75} mt="2" ml="-2.5" fontSize="sm">
+        75km
+      </SliderMark>
 
-    <SliderTrack>
-      <SliderFilledTrack />
-    </SliderTrack>
-    <SliderThumb boxSize={6}>
-      {sliderValue}
-    </SliderThumb>
-
+      <SliderTrack>
+        <SliderFilledTrack />
+      </SliderTrack>
+      <SliderThumb boxSize={6}>{sliderValue}</SliderThumb>
     </Slider>
   );
 };
