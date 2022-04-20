be48af205cbc653be6e0bf4bffdf34482f2a8423
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-04-19 19:50:45 -0700

    prettier

--- components/carousel/App.js
+++ components/carousel/App.js
@@ -1,10 +1,9 @@
-import '../../styles/globals.css'
+import "../../styles/globals.css";
 import { Box } from "@chakra-ui/react";
 import ImageSlider from "./ImageSlider";
 import { SlideData } from "./SlideData";
 
 export default function ImageSliderCustom() {
-
   return (
     <Box w="100%" p={0} m={0} color="white">
       <ImageSlider key={SlideData.key} slides={SlideData} />
