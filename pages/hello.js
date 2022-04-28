297eeddfaf5f8d18071ffce7dd166e424b35ce8f
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-04-27 20:53:41 -0700

    learning tsc

--- pages/hello.js
+++ pages/hello.js
@@ -1,1 +1,5 @@
-console.log("Hello world!");
+// This is an industrial-grade general-purpose greeter function:
+function greet(person, date) {
+    console.log("Hello ".concat(person, ", today is ").concat(date, "!"));
+}
+greet("Brendan");
--- pages/hello.ts
+++ pages/hello.ts
@@ -1,1 +1,5 @@
-console.log("Hello world!");
+function greet(person: string, date: Date) {
+  console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+ 
+greet("Maddison", new Date());
